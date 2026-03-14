a05755b81f3eb75b946ee6f59fd77e9b9e2245b9
Dragonborn9 <152669423+Dragonborn9@users.noreply.github.com>
2026-03-14 18:06:31 -0300

    primeira versão calculadora simples

---
+++ Calculadora_Simples.py
@@ -0,0 +1,27 @@
+print("Calculadora Simples v1")
+
+valor1 = float(input("Informe o valor 1: "))
+valor2 = float(input("Informe o valor 2: "))
+op = str(input("Informe a operação (+, -, /, *, ^):  "))
+
+if op == "+" or op == "-" or op == "*" or op == "/" or op == "^":
+    if op == "/":
+        if valor2 == 0:
+            print("Não é possível dividir por 0")
+        else:
+            resultado = valor1 / valor2
+            print("Resultado: ", resultado)
+    if op == "+":
+        resultado = valor1 + valor2
+        print("Resultado: ", resultado)
+    if op == "-":
+        resultado = valor1 - valor2
+        print("Resultado: ", resultado)
+    if op == "*":
+        resultado = valor1 * valor2
+        print("Resultado: ", resultado)
+    if op == "^":
+        resultado = valor1 ** valor2
+        print("Resultado: ", resultado)
+else:
+    print("Operador Inválido")
